0bb7ad2677b935090fe0f2710bf99da030ca93db
jpabloxx <jpablox117@hotmail.com>
2016-07-11 22:55:49 -0500

    no sirve mostrar vendedores

--- menus/visualizarpedidos.php
+++ menus/visualizarpedidos.php
@@ -1,4 +1,22 @@
+<?php
+    include '../conexion.php';
+    $sql="SELECT ven_codigo,ven_nombre FROM vendedores";    
+    $result = $mysqli->query($sql);
+    $result->data_seek(0);
+    while ($fila = $result->fetch_assoc()) {
+        $ven_codigo[]=$fila['ven_codigo'];
+        $ven_nombre[]=$fila['ven_nombre'];
+    }
+    $sql="SELECT cli_cedula,cli_nombre FROM clientes";    
+    $result = $mysqli->query($sql);
+    $result->data_seek(0);
+    while ($fila = $result->fetch_assoc()) {
+        $cli_cedula[]=$fila['cli_cedula'];
+        $cli_nombre[]=$fila['cli_nombre'];
+    }
 
+
+?>
 <!doctype html>
 <html><!-- InstanceBegin template="/Templates/template_admin.dwt.php" codeOutsideHTMLIsLocked="false" -->
 <head>
@@ -37,12 +55,11 @@
         <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
         <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
-
 </head>
 
 
 
-<body>
+<body onload="listar();">
 
 <div id="wrapper">
 
@@ -185,13 +202,13 @@
                     <div class="col-lg-12">
                       <h1 class="page-header">Ver Pedidos</h1>
                     </div>
-                    <form>
+                    <form name="form1" id="form1">
                          <label>Fecha Desde: </label>
                         <input  type="text" class="form-control input-sm"   name="fechainicial" id="fecha1" style="cursor: hand;width:20%;" required readonly="readonly"> 
                         <label>Fecha Hasta: </label>
                         <input  type="text" class="form-control input-sm"   name="fechafinal" id="fecha2" style="cursor: hand;width:20%;" required readonly="readonly">
                         <label class=" control-label" for="vendedor" >Vendedor : </label>   
-                                    <select class="form-control  input-sm" name="vendedor" required style="width:20%;">
+                                    <select class="form-control  input-sm" name="vendedor" id="vendedor" required style="width:20%;">
                                     </select>
                          <label class=" control-label" for="vendedor" >Cliente : </label>   
                                     <select class="form-control  input-sm" name="vendedor" required style="width:20%;">
@@ -216,7 +233,6 @@
 
     <!-- jQuery 
     <script src="../bootstrap/template01/bower_components/jquery/dist/jquery.min.js"></script>
-
     < Bootstrap Core JavaScript 
     <script src="../bootstrap/template01/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
     
@@ -230,8 +246,29 @@
 
 <script type="text/javascript">
 
-    $( document ).ready(function() {
-            $('#fecha1').datepicker();
-            $('#fecha2').datepicker();
-        });
+
+function listar(){
+         var a = new Array();
+          var cont =0;
+        <?php
+        for ($i = 0, $total = count($ven_nombre); $i < $total; $i ++) {               
+            echo "\na[$i] = '$ven_nombre[$i]';";
+         }
+         ?>
+        form1.vendedor.length=0;
+        for (var i = 0; i < a.length; i++) {            
+             opcion0 = new Option(a[i],i+1);
+             document.forms.form1.vendedor.options[cont]=opcion0;
+             cont++;         
+         }
+}
+
+
+
+
+
+ $( document ).ready(function() {
+         $('#fecha1').datepicker();
+         $('#fecha2').datepicker();
+     });
 </script>
