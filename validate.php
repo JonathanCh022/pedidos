d0a3661e060595c48c0701cde918d6d2ca54390f
JonathanCh022 <chacon_022@hotmail.com>
2016-07-11 21:31:37 -0500

    ya tira mensajes de error si falla. "generico".

--- validate.php
+++ validate.php
@@ -4,19 +4,32 @@
 	$usuario=$_POST['Usuario'];
 	$pass=$_POST['Password'];
 
-	$sql="SELECT usu_nombre FROM usuarios WHERE usu_nombre = '$usuario' AND usu_clave = '$pass'";	
+	$sql="SELECT usu_nit,usu_nombre,usu_clave,usu_rol FROM usuarios WHERE usu_nit = '$usuario' AND usu_clave = '$pass'";	
     $result = $mysqli->query($sql);
 
+    $sql2="SELECT emp_nit,emp_raz_soc,emp_email,emp_telefono FROM empresa WHERE emp_nit = '$empresa'";	
+    $result2 = $mysqli->query($sql2);
 
-	if (mysqli_num_rows($result) > 0) {
-						
-   					 while($row = mysqli_fetch_assoc($result)) {
-   					 	session_start();
-   					 $_SESSION['name']=$row['usu_nombre'];
-   					 header('Location: vista_general.php');
+
+	if (mysqli_num_rows($result) > 0 && mysqli_num_rows($result2) > 0) {
+					session_start();
+					 while($row = mysqli_fetch_assoc($result2)) {	   					 
+	   					 $_SESSION['emp_nit']=$row['emp_nit'];
+	   					 $_SESSION['emp_raz_soc']=$row['emp_raz_soc'];
+	   					 $_SESSION['emp_email']=$row['emp_email'];
+	   					 $_SESSION['emp_telefono']=$row['emp_telefono'];
+	   					 
    					 }
+
+   					 while($row1 = mysqli_fetch_assoc($result)) {	   					 
+	   					 $_SESSION['usu_nit']=$row1['usu_nit'];
+	   					 $_SESSION['usu_name']=$row1['usu_nombre'];
+	   					 $_SESSION['usu_clave']=$row1['usu_clave'];
+	   					 $_SESSION['usu_rol']=$row1['usu_rol'];
+	   					 
+   					 }header('Location: menus/');
    					}else{
-   						header('Location: ../pedidos/login.php?errorusuario=si');
+   						header('Location: ../pedidos/index.php?errorusuario=si');
    					}
 ?>	
 
