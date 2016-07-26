3bb74900c59123ad10987a2d8edeff785d1a6173
jpabloxx <jpablox117@hotmail.com>
2016-07-26 12:57:13 -0500

    menu empresa

---
+++ menus/procempresa.php
@@ -0,0 +1,18 @@
+<?php
+error_reporting(0);
+ini_set('display_errors', 0);
+header("Content-Type: text/html;charset=utf-8");
+    session_start();
+    if (!isset($_SESSION['usu_nit']) ) {
+       header('Location: /pedidos/index.php?errorusuario=si');
+       exit();
+    }
+  include '../conexion.php';
+
+  $sqlgeneral="UPDATE empresa SET emp_raz_soc='".$_POST['rz']."', emp_email='".$_POST['mail']."',emp_telefono='".$_POST['telefono']."' WHERE emp_nit='".$_SESSION['emp_nit']."'";
+  if ($mysqli->query($sqlgeneral) === TRUE) {
+  	
+  }else{
+  	echo "Error: " . $sqlgeneral . "<br>" . $mysqli->error;
+  }
+  ?>	
